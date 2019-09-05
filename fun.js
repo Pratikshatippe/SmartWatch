8b56b9d52dab6a5021524efabec2a7bb7f8d91b6
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-05 23:12:50 +0530

    Add icon

---
+++ fun.js
@@ -0,0 +1,9 @@
+// Javascript file for smart watch application
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+document.getElementById("day").innerHTML = daylist[day];
+var time1 = (
+today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+)
+document.getElementById("time").innerHTML = time1;
