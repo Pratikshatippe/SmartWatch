d65d8acadc597e6874ceed4da74532c5302391c0
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-06 16:49:01 +0530

    message list displayed

--- fun.js
+++ fun.js
@@ -3,15 +3,22 @@ var today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
 document.getElementById("day").innerHTML = daylist[day];
-var time1 = (
-today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-)
+var hours = today.getHours();
+if(hours>12){
+    hours = hours-12;
+}
+var minute = today.getMinutes();
+var time1 = (hours+":"+minute);
 document.getElementById("time").innerHTML = time1;
 
-const messagelist = ["VZ-VFCARE","VM-Career","VM-SWIGGY","VK-SBIUPI"];
-console.log(messagelist);
-document.getElementById("chat").innerHTML=messagelist;
+// Message
 
+const messagelist = ["VZ-VFCARE","VM-Career","VM-SWIGGY","VK-SBIUPI"];
+
+document.getElementById("msg-1").innerHTML=messagelist[0];
+document.getElementById("msg-2").innerHTML=messagelist[1];
+document.getElementById("msg-3").innerHTML=messagelist[2];
+document.getElementById("msg-4").innerHTML=messagelist[3];
 // show and hide
 $(document).ready(function(){
     $(".main").show();
@@ -41,4 +48,4 @@ $(document).ready(function(){
     });
 });
 
-// Message
+
