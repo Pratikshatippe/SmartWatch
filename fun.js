28753bee1b395ea911d8095a9bb62fc7bb461f17
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-07 09:21:48 +0530

    updated

--- fun.js
+++ fun.js
@@ -10,15 +10,17 @@ if(hours>12){
 var minute = today.getMinutes();
 var time1 = (hours+":"+minute);
 document.getElementById("time").innerHTML = time1;
-
+document.getElementById("top").innerHTML = time1;
+document.getElementById("up").innerHTML = time1;
+document.getElementById("btm").innerHTML = time1;
 // Message
 
-const messagelist = ["VZ-VFCARE","VM-Career","VM-SWIGGY"];
+const messagelist = ["VZ-VFCARE","VM-Career","VM-SWIGGY","VZ-SBIUPI"];
 
 document.getElementById("msg-1").innerHTML=messagelist[0];
 document.getElementById("msg-2").innerHTML=messagelist[1];
 document.getElementById("msg-3").innerHTML=messagelist[2];
-// document.getElementById("msg-4").innerHTML=messagelist[3];
+document.getElementById("msg-4").innerHTML=messagelist[3];
 // show and hide
 $(document).ready(function(){
     $(".main").show();
@@ -31,6 +33,13 @@ $(document).ready(function(){
         $(".mainMsg").show();
         $(".mainMusic").hide();
         $(".mainTimer").hide();  
+    });
+    $("#msg-1").click(function(){
+        $(".main").hide();
+        $(".mainMsg").hide();
+        $(".mainMusic").hide();
+        $(".mainTimer").hide(); 
+        $ ("#msg-1").show();
     });
 
     $("#music").click(function(){
