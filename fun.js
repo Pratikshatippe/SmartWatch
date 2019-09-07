12c5d51cf66b5564e3f202d62ea2ad5e4eb05708
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-07 09:45:19 +0530

    timer

--- fun.js
+++ fun.js
@@ -21,6 +21,72 @@ document.getElementById("msg-1").innerHTML=messagelist[0];
 document.getElementById("msg-2").innerHTML=messagelist[1];
 document.getElementById("msg-3").innerHTML=messagelist[2];
 document.getElementById("msg-4").innerHTML=messagelist[3];
+
+// Timer
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('start');
+var watch = new Stopwatch(timer);
+
+function start() {
+    toggleBtn.textContent = 'Stop';
+    watch.start();
+  }
+  function stop() {
+    toggleBtn.textContent = 'Start';
+    watch.stop();
+  }
+  toggleBtn.addEventListener('click', function() {
+    watch.isOn ? stop() : start();
+  });
+
+function Stopwatch(elem) {
+    var time = 0;
+    var offset;
+    var interval;
+  
+    function update() {
+      if (this.isOn) {
+        time += delta();
+      }
+      elem.textContent = timeFormatter(time);
+    }
+    function delta() {
+      var now = Date.now();
+      var timePassed = now - offset;
+  
+      offset = now;
+      return timePassed;
+    }
+    function timeFormatter(time) {
+      time = new Date(time);
+  
+      var hours = time.getHours().toString();
+      var minutes= time.getMinutes().toString();
+      var seconds = time.getSeconds().toString();
+  
+      if (hours.length < 2) {
+        hours = '0' + hours; 
+      }
+      if (minutes.length < 2) {
+        minutes = '0' + minutes;
+      }
+      while (seconds.length < 2) {
+        seconds = '0' + seconds;
+      }
+      return hours + ' : ' + minutes + ' . ' + seconds;
+    }
+    this.start = function() {
+      interval = setInterval(update.bind(this), 10);
+      offset = Date.now();
+      this.isOn = true;
+    };
+    this.stop = function() {
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    };
+    this.isOn = false;
+  }
 // show and hide
 $(document).ready(function(){
     $(".main").show();
